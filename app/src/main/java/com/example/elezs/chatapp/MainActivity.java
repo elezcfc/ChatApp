cc846771fc5ee54df9c0a550af68afdef90b9d51
elezs <stefanchero@gmail.com>
2018-09-19 23:07:28 +0200

    Update build tools and gradle

--- app/src/main/java/com/example/elezs/chatapp/MainActivity.java
+++ app/src/main/java/com/example/elezs/chatapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.elezs.chatapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.*;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
